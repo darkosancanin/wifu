ab8c147e30b59b7ed896179d26c2c233b1cfdf4a
Darko Sancanin <darko.sancanin@gmail.com>
2015-06-11 18:57:48 +1000

    Removed pyshark library from repo.

--- pyshark/config.py
+++
@@ -1,8 +0,0 @@
-import pyshark
-import py
-import os
-
-CONFIG_PATH = os.path.join(os.path.dirname(pyshark.__file__), 'config.ini')
-
-def get_config():
-    return py.iniconfig.IniConfig(CONFIG_PATH)
